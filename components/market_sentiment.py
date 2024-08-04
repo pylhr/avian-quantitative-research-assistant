be384a666c7ac190e1ed034fd6050817d2720b5c
priyanshu lohar <priyanshulohar1@gmail.com>
2024-08-05 02:41:03 +0530

    market sentiment modified

--- components/market_sentiment.py
+++ components/market_sentiment.py
@@ -1,6 +1,26 @@
 # components/market_sentiment.py
 import streamlit as st
 from utils.ai71_client import get_llm_response
+from utils.search_client import search_result
+
+
+def sentiment_analysis(query):
+    data_input = search_result(query)
+    prompt = (
+        f"Based on the following real-time data, provide a precise prediction and market sentiment analysis for the provided company."
+        f"Include relevant sources and links if needed.\n\n{data_input}"
+    )
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful quantitative and financial research assistant. Provide a precise prediction and market sentiment analysis based on the real-time data, including sources like news articles.",
+        },
+        {"role": "user", "content": prompt},
+    ]
+    response = get_llm_response("tiiuae/falcon-180B-chat", messages)
+
+    return response
 
 
 def market_sentiment_page():
@@ -13,10 +33,7 @@ def market_sentiment_page():
     ticker = st.text_input("Enter stock ticker")
     if st.button("Analyze Sentiment", key="sentiment"):
         st.info(f"Analyzing sentiment for {ticker}...")
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Analyze the market sentiment for {ticker}."},
-        ]
-        response = get_llm_response("tiiuae/falcon-180B-chat", messages)
+
+        response = sentiment_analysis(ticker)
         st.success("Sentiment analysis complete!")
         st.write(response)
